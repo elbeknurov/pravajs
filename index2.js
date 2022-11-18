1319815575267e6506e4fa3022f0012ee8b7c5b8
elbeknurov <elbeknurov11@gmail.com>
2022-11-18 15:33:07 +0500

    pravajs

---
+++ index2.js
@@ -0,0 +1,25 @@
+// // 1 misol
+// alert(`Assalomu aleykum prava olish uchun ariza qoldirinng!!!`);
+// var name = prompt("Ismingizni kiriting");
+// var age = +prompt(`${name} Yoshingizni kiriting`);
+
+// if (age >= 18) {
+//   alert(`${name} siz prava olish yoshidasiz `);
+// } else {
+//   alert(`sizga prava olish uchun ${18 - age} yil kerak ekan!!!`);
+// }
+
+// // 2 misol
+// var a = +prompt("1sonni kiriting?");
+// var b = +prompt("2sonni kiriting?");
+// var c = +prompt("3sonni kiriting?");
+
+// if (a > b && a > c) {
+//   console.log(`${a} soningiz katta`);
+// } else if (b > c) {
+//   console.log(`${b}  soningiz katta`);
+// } else if (a == b && a == c && b == c) {
+//   console.log("siz hamma soningizni bir xil kiritdingiz");
+// } else {
+//   console.log(`${c} soningiz katta`);
+// }
